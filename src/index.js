a1d042b098d72aa407bb329af340f1c01f8c266d
Rahul Kottak <rahulkottak@gmail.com>
2023-09-21 18:33:35 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const { PORT } = require('./config/serverConfig');
+
+const setUpAndStartServer = async () => {
+    
+    //create the xpress object
+    const app = express();
+
+    const PORT = 4000;
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+
+    });
+
+}
+
+setUpAndStartServer();

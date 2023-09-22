ba3c0aa80eb4b4e07a87982e035e50561be6a60f
Rahul Kottak <rahulkottak@gmail.com>
2023-09-22 12:21:21 +0530

    basic Update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const { PORT } = require('./config/serverConfig');
 
+require('dotenv').config();
+
 const setUpAndStartServer = async () => {
     
     //create the xpress object
     const app = express();
 
-    const PORT = 3000;
+    const PORT = process.env.PORT ;
 
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`)
